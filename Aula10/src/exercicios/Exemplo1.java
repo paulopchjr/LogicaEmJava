5411d5abeb7473307629ca26dd965542f4bfe901
paulopchjr <paulopchjr@gmail.com>
2023-08-31 15:53:58 -0300

    Divisões e resto com MOD do Java

---
+++ Aula10/src/exercicios/Exemplo1.java
@@ -0,0 +1,23 @@
+package exercicios;
+
+public class Exemplo1 {
+	
+	public static void main(String[] args) {
+		
+		
+		for(int x=1; x <=33; x++) {
+			
+			if(x % 2 == 0) {
+				
+				System.out.println("Número "+x + " é par");
+				
+			}else {
+				
+				System.out.println("Número é "+ x +" impar " );
+			}
+			
+			
+		}
+	}
+
+}
---
+++ Aula10/src/exercicios/Exemplo2.java
@@ -0,0 +1,21 @@
+
+package exercicios;
+
+public class Exemplo2 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		double laranjas = 9;
+		
+		double pessoas = 2;
+		
+		double resto = laranjas % pessoas;
+		
+		
+		System.out.println("A sobra da divisao de "+laranjas+" laranjas para "+ pessoas+" pessoas é de ="+resto);
+		
+		
+	}
+}
