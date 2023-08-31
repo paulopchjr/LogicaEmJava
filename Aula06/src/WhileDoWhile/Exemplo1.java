c9fc346172153923c59fc18b847e64a30987ba61
paulopchjr <paulopchjr@gmail.com>
2023-08-31 15:49:45 -0300

    Estrutura de Repetição WHILE e DO WHILE

---
+++ Aula06/src/WhileDoWhile/Exemplo1.java
@@ -0,0 +1,15 @@
+package WhileDoWhile;
+
+public class Exemplo1 {
+
+	public static void main(String[] args) {
+
+		int numero = 0;
+		while (numero < 100) {
+			numero ++;
+			System.out.println(numero);
+		}
+
+	}
+
+}
---
+++ Aula06/src/WhileDoWhile/Exemplo2.java
@@ -0,0 +1,18 @@
+package WhileDoWhile;
+
+public class Exemplo2 {
+
+	public static void main(String[] args) {
+		
+		
+		int numero =0;
+		
+		do { // executa depois verifica
+			
+			
+			System.out.println(numero);
+			numero ++;
+			
+		} while (numero <= 100);
+	}
+}
